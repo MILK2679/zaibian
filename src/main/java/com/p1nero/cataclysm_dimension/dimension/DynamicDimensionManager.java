f0820163072e3bd7552d6dae410a43754e46ac23
milk <114768035+MILK2679@users.noreply.github.com>
2026-04-18 04:33:08 +0800

    Update DynamicDimensionManager.java

--- src/main/java/com/p1nero/cataclysm_dimension/dimension/DynamicDimensionManager.java
+++ src/main/java/com/p1nero/cataclysm_dimension/dimension/DynamicDimensionManager.java
@@ -6,7 +6,7 @@ import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.MinecraftServer;
-import net.minecraft.server.level.ChunkProgressListener;
+import net.minecraft.server.level.progress.ChunkProgressListener;
 import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.biome.BiomeManager;
